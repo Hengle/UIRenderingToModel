53fb3d8838340237b7aff109d2b02d7bf3b71354
王祖雄 <wangzuxiong@galasports.net>
2019-09-20 15:51:16 +0800

    first

---
+++ Assets/ObjRotate.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ObjRotate : MonoBehaviour
+{
+    public float Speed = -2;
+    private void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, Speed);
+    }
+}
